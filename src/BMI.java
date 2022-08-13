9555b1ebcc02b084ff543213e3c99ecd6037d5d4
Станислав <diezz_92@mail.ru>
2022-08-13 20:13:17 +0500

    fixed #1

--- src/BMI.java
+++ src/BMI.java
@@ -1,8 +1,9 @@
 public class BMI {
     public static void main(String[] args) {
         BmiService service = new BmiService();
+        double high = 170;
         double mass = 77;
-        double result = service.calculate(mass);
+        double result = service.calculate(mass, high);
         System.out.println(result);
 
     }
--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,5 @@
 public class BmiService {
-    public double calculate (double mass) {
-        double high = 170;
+    public double calculate (double mass, double high) {
         double x = high / 100 * 2;
         double result = mass / x;
         return result;
